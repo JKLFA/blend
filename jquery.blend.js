27904e7ee7cd0b11084dbf6f0f058e281924f5a8
Jack Moore <jack@colorpowered.com>
2010-07-24 12:19:18 -0500

    1.3 - Jan 28 2010

--- jquery.blend.js
+++ jquery.blend.js
@@ -1,5 +1,5 @@
 /*
-	jQuery Blend v1.1
+	jQuery Blend v1.3
 	(c) 2009 Jack Moore - www.colorpowered.com - jack@colorpowered.com
 	Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 */
@@ -12,12 +12,14 @@
     */
     
     $.fn.blend = function(options, callback) {
+	// Disable Blend for FireFox 2 because of a bug that does not allow JavaScript
+	// to retrieve the CSS background position.
+	// More info: https://bugzilla.mozilla.org/show_bug.cgi?id=316981
+	if ($.browser.mozilla && (parseFloat($.browser.version) < 1.9)) { return this; }
 	
 	var settings = $.extend({}, $.fn.blend.settings, options);
-        
-        $(this).each(function(){
-            
-            var $this = $(this),
+        $(this).each(function(){    
+	    var $this = $(this),
 		$target = $(settings.target ? settings.target : this),
 		$hover,
 		target = [],
@@ -27,25 +29,26 @@
 		active = false,
 		out = 0,
 		opacity = settings.opacity,
+		bg = 'background-',
 		properties = [
-		    'background-color',
-		    'background-image',
-		    'background-repeat',
-		    'background-attachment',
-		    'background-position',
-		    'background-position-x',
-		    'background-position-y'
+		    bg+'color',
+		    bg+'image',
+		    bg+'repeat',
+		    bg+'attachment',
+		    bg+'position',
+		    bg+'position-x',
+		    bg+'position-y'
 		];
 	    
 	    length = properties.length;
-            
-            if($target[0].style.position != 'absolute'){
-                $target.css({position:'relative'});   
-            }
-            
-            if(!$target.hasClass('hover')){
-                $target.wrapInner('<div style="position:relative" />');
-            }
+	    
+	    if($target[0].style.position !== 'absolute'){
+		$target.css({position:'relative'});   
+	    }
+	    
+	    if(!$target.hasClass('hover')){
+		$target.wrapInner('<div style="position:relative" />');
+	    }
 	    
 	    for (i=0; i<length; i++){
 		target[i] = $target.css(properties[i]);
@@ -57,66 +60,66 @@
 		style.position='absolute';
 		style.top=0;
 		style.left=0;
-		style.width=$target.css('width');
-		style.height=$target.css('height');
+		style.width=$target.width();
+		style.height=$target.height();
 		for (i=0; i<length; i++){
 		    style[properties[i]] = $target.css(properties[i]);
 		}
-	    
+		
 	    //checks to see if blend has already been applied to an element.
-            if($target.find(".jsblend").length === 0){
-                $hover = $('<div class="jsblend" />').css(style);
+	    if($target.find(".jsblend").length === 0){
+		$hover = $('<div class="jsblend" />').css(style);
 		
 		if(settings.top){
 		    $hover.appendTo($target);
 		} else {
 		    $hover.prependTo($target);
 		}
-            } else {
-                $hover = $target.find(".jsblend");
-            }
+	    } else {
+		$hover = $target.find(".jsblend");
+	    }
 	    
 	    style = {};
-		for (i=0; i<length; i++){
-		    style[properties[i]] = target[i];
-		}
+	    for (i=0; i<length; i++){
+		style[properties[i]] = target[i];
+	    }
 	    
 	    $target.css(style);
-            
-            if(settings.reverse){
-                out = settings.opacity;
-                opacity = 0;
-            }
-            $hover.css({opacity:out});
-            
-            function pulse(o){
-                if(active){
-                    $hover.fadeTo(settings.speed, o, function(){
-                        pulse(o==out?opacity:out);
-                    });
-                }
-            }
 	    
-            if(settings.pulse && settings.active){
-                active = true;
-                pulse(opacity);
+	    if(settings.reverse){
+		out = settings.opacity;
+		opacity = 0;
+	    }
+	    $hover.css({opacity:out});
+	    
+	    function pulse(o){
+		if(active){
+		    $hover.fadeTo(settings.speed, o, function(){
+			pulse(o===out?opacity:out);
+		    });
+		}
+	    }
+	    
+	    if(settings.pulse && settings.active){
+		active = true;
+		pulse(opacity);
 	    } else if(settings.pulse){
-                $this.hover(function(){
-                    active = true;
-                    pulse(opacity);
-                }, function(){
-                    active = false;
-                    $hover.stop(true).fadeTo(settings.speed, out);
-                });
-            } else {
-                $this.hover(function(){
-                    $hover.stop().fadeTo(settings.speed, opacity);
-                }, function(){
-                    $hover.stop().fadeTo(settings.speed, out);
-                });
-            }
+		$this.hover(function(){
+		    active = true;
+		    pulse(opacity);
+		}, function(){
+		    active = false;
+		    $hover.stop(true).fadeTo(settings.speed, out);
+		});
+	    } else {
+		$this.hover(function(){
+		    $hover.stop().fadeTo(settings.speed, opacity);
+		}, function(){
+		    $hover.stop().fadeTo(settings.speed, out);
+		});
+	    }
 	    
-        });
+	});
         return this;
     };
     
@@ -130,4 +133,4 @@
 	top : false
     };
     
-})(jQuery);
+}(jQuery));
